7acb2a3bbac33b1d8b2eaee0859bab1aff8ca927
Adhika Guna <adhikag167@gmail.com>
2020-07-08 17:17:38 +0700

    menambahkan file index

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Project Statistics</title>
+</head>
+<body>
+<h2>Final Project Probability and Statistics</h2>
+    <form action="Control.php">
+        <select name="operation" id="operation">
+            <option value="intro">Intro to Probability</option>
+            <option value="conditional">Conditional</option>
+            <option value="bayes">Bayes</option>
+        </select>
+    <button type="submit">Apply</button>
+    </form>
+    
+</body>
+</html>
